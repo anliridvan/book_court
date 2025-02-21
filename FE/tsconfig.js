079453b096cc1e111ced91557d37ade06ac2f27c
anliridvan <anliridvan@hotmail.com>
2025-02-21 14:35:54 +0300

    root folder init

---
+++ FE/tsconfig.js
@@ -0,0 +1,13 @@
+{
+    "compilerOptions": {
+        "target": "es2015",
+        "module": "esnext",
+        "strict": true,
+        "noImplicitAny": true,
+        "moduleResolution": "node",
+        "baseUrl": "./",
+        "paths": {
+            "@app/*": ["src/app/*"]
+        }
+    }
+}
